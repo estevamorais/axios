98f643e9fec10bb86fd7022dca305f710c65fea2
estevamorais <demoraisestevam@gmail.com>
2023-03-21 19:29:07 -0300

    Create get request with error and handling error

--- src/js/main.js
+++ src/js/main.js
@@ -96,7 +96,29 @@ const transform = () => {
 }
 
 const errorHandling = () => {
-	console.log('errorHandling');
+	// Forçando error (404)
+	axios.get('https://jsonplaceholder.typicode.com/forceerror')
+		.then((response) => renderOutput(response))
+		.catch((error) => {
+			renderOutput(error)
+
+			if (error.response) {
+				// A requisição foi feita e o servidor respondeu com um código de status
+				// que sai do alcance de 2xx
+				console.error(error.response.data);
+				console.error(error.response.status);
+				console.error(error.response.headers);
+			} else if (error.request) {
+				// A requisição foi feita mas nenhuma resposta foi recebida
+				// `error.request` é uma instância do XMLHttpRequest no navegador e uma instância de
+				// http.ClientRequest no node.js
+				console.error(error.request);
+			} else {
+				// Alguma coisa acontenceu ao configurar a requisição que acionou este erro.
+				console.error('Error', error.message);
+			}
+			console.error(error.config);
+		});
 }
 
 const cancel = () => {
